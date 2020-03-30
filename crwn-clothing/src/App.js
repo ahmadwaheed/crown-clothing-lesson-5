c8f913c5c56a2c63253ba017c76bcfa725958d49
Ahmad Waheed <ahmadwaheedsalimi@gmail.com>
2020-03-30 00:46:27 -0700

    Added /shop and styling

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
+
+//exact: if we use exact within the Route, it will derender that component when we are trying to render other components.
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
